140e95bd6fee168a7da83eb27e3b4c27b5fa937a
Thor77 <xXThor77Xx@gmail.com>
2014-10-15 13:39:28 +0200

    Initial commit

---
+++ cc_platform.py
@@ -0,0 +1,10 @@
+class Platform:
+
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        return None
+
+    def get_name(self):
+        return self.__class__.__name__
---
+++ main.py
@@ -0,0 +1,44 @@
+#########   #########   #########   #    ######    ##   #    ####
+#       #   #       #       #       #    #    #    # #  #    #
+#       #   #       #       #       #    #    #    # #  #    #
+#       #   #########       #       #    #    #    #  # #    ####
+#       #   #               #       #    #    #    #  # #       #
+#       #   #               #       #    #    #    #   ##       #
+#########   #               #       #    ######    #    #    ####
+
+#################################################################
+
+host = 'localhost'
+port = 8080
+
+######  #####   ####    #####
+#       #   #   #  ##   #
+#       #   #   #   #   #
+#       #   #   #   #   #####
+#       #   #   #   #   #
+#       #   #   #  ##   #
+######  #####   ####    #####
+
+#############################
+from pluginfileloader import PluginFileLoader
+from cc_platform import Platform
+from bottle import route, run, template
+import json
+
+cc_platforms = []
+
+@route('/')
+def get_data():
+    data = {}
+    for pl in cc_platforms:
+        d = pl.get_data()
+        if d:
+            data[pl.get_name()] = pl.get_data()
+    return json.dumps(data)
+
+for plugin in PluginFileLoader('cc_platforms', Platform):
+    pl = plugin()
+    cc_platforms.append(pl)
+
+#route(url, callback=func)
+run(host=host, port=8080)
